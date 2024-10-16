71d0549549f1c2f376e4db2cc6502033c9f302de
avin <esmaili_avin@yahoo.com>
2024-10-16 15:50:41 +0330

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// function App(){
+//     function clickHandler(){
+//         console.log('wooow')
+//     }
+//     return(
+//         <button onClick={clickHandler}>Click me plz</button>
+//     )
+// }
+
+// function App(){
+//     let clickHandler = () => {
+//         console.log('wooow')
+//     }
+//     return(
+//         <button onClick={clickHandler}>Click me plz</button>
+//     )
+// }
+
+
+// class App extends React.Component {
+//     clickHandler() {
+//         console.log('wooow greeat code')
+//     }
+//     render(){
+//     return(
+//         <button onClick={this.clickHandler}>Click me plz</button>
+//     )
+// }
+// }
+
+
+// function App(){
+//     const clickHandler = () => {
+//         console.log('avin esmaeili')
+//     }
+//     return(
+//         <button onClick={clickHandler()}>Click MEEE</button>
+//     )
+// }
+
+// function App(){
+//     const clickHandler = (name) => {
+//         console.log(`hellooo ${name}`)
+//     }
+//     return(
+//         <button onClick={clickHandler('avin')}>Click MEEE</button>
+//     )
+// }
+
+// function App(){
+//     const clickHandler = (name) => {
+//         console.log(`hellooo ${name}`)
+//     }
+//     return(
+//         <button onClick={()=>{clickHandler('avin')}}>Click MEEE</button>
+//     )
+// }
+
+// function App(){
+//     const clickHandler = (name) => {
+//         console.log(`hellooo ${name}`)
+//     }
+//     return(
+//         <button onClick={()=>clickHandler('avin')}>Click MEEE</button>
+//     )
+// }
+
+
+// class App extends React.Component {
+//     clickHandler(name){
+//         console.log(`click done by ${name}`)
+//     }
+//     render(){
+//         return(
+//             <button onClick={this.clickHandler('avin')}>hello world</button>
+//         )
+//     }
+// }
+
+// class App extends React.Component {
+//     clickHandler(name){
+//         console.log(`click done by ${name}`)
+//     }
+//     render(){
+//         return(
+//             <button onClick={()=>this.clickHandler('avin')}>hello world</button>
+//         )
+//     }
+// }
+
+
+// function App(){
+//     const btnClick = (event)=>{
+//         console.log(event.target)
+//         console.log(event.type)
+//     }
+//     return(
+//         <button onClick={btnClick}>Clickkkk</button>
+//     )
+// }
+
+// function App(){
+//     const btnClick = (event)=>{
+//         console.log(event.target)
+//         console.log(event.type)
+//     }
+//     return(
+//         <button onClick={btnClick}>Clickkkk</button>
+//     )
+// }
+
+// function App(){
+//     function btnClick(event){
+//         console.log(event.target)
+//         console.log(event.type)
+//     }
+//     return(
+//         <button onClick={btnClick}>Clickkkk</button>
+//     )
+// }
+
+
+// function App(){
+//     function btnClick(event){
+//         console.log(event.target)
+//         console.log(event.type)
+//     }
+//     return(
+//         <button onClick={(event)=>{btnClick(event)}}>Clickkkk</button>
+//     )
+// }
+
+// function App(){
+//     function btnClick(event){
+//         console.log(event.target)
+//         console.log(event.type)
+//     }
+//     return(
+//         <button onClick={(event)=>btnClick(event)}>Clickkkk</button>
+//     )
+// }
+
+// function App(){
+//     const btnClick = (event)=>{
+//         console.log(event.target)
+//         console.log(event.type)
+//     }
+//     return(
+//         <button onClick={(event)=> btnClick(event)}>Clickkkk</button>
+//     )
+// }
+
+
+
+
+export default App
